f3b1ed0ae0e810e750291441aef603d36f32e25e
Latosha Tims <ltims080913@hotmail.com>
2020-05-22 09:35:39 -0700

    adjusted server for cookies session

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session')
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -8,6 +9,19 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+const sessionConfig = {
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: process.send.SECURE_COOKIE || false, //send cookie over HTTPS only, should be true during production
+        httpOnly: true, //true means that clients JS cannot access the cookie
+    },
+    resave: false,
+    saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
+    name: 'Name',
+    secret: process.env.COOKIE_SECRET || 'secretcookie'
+}
+
+server.use(session(sessionConfig))
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
